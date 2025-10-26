5763d6c75eefe426222b53f8e5b85abcf66f6015
TheMAZ2-png <csm012004@outlook.com>
2025-10-26 09:01:43 -0400

    Initial commit

---
+++ lootLogger/ItemCell.swift
@@ -0,0 +1,12 @@
+//
+//  ItemCell.swift
+//  lootLogger
+//
+//  Created by Chris Mazurek on 10/26/25.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+}
---
+++ lootLoggerTests/lootLoggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  lootLoggerTests.swift
+//  lootLoggerTests
+//
+//  Created by Chris Mazurek on 10/21/25.
+//
+
+import Testing
+@testable import lootLogger
+
+struct lootLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
