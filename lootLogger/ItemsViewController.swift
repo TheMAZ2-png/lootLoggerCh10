8eb4b26b4e4b93a2cc5e0f5cec396f73ed73a1bd
TheMAZ2-png <csm012004@outlook.com>
2025-10-26 09:18:12 -0400

    Commit

--- lootLogger/ItemsViewController.swift
+++ lootLogger/ItemsViewController.swift
@@ -48,23 +48,26 @@ class ItemsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView,
-            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        // Create an instance of UITableViewCell, with default appearance
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
-        
-        
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a new or recycled cell
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell",
+                                                 for: indexPath) as! ItemCell
 
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
-        // will appear in on the table view
+        // will appear in on the tableview
         let item = itemStore.allItems[indexPath.row]
 
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+
+        // Configure the cell with the Item
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
 
         return cell
     }
+
     
     override func tableView(_ tableView: UITableView,
                             commit editingStyle: UITableViewCell.EditingStyle,
@@ -88,5 +91,13 @@ class ItemsViewController: UITableViewController {
         // Update the model
         itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.rowHeight = 65
+    }
+    
+    
+    
     
 }
